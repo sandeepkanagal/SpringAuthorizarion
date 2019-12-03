7130c79ebf4a6603f2a6d72aa7fe08247ccd2a2c
Sandeep Kanagal <sandeep.kanagal@in.ibm.com>
2019-12-03 23:39:51 +0530

    first commit

---
+++ boot-resource-server/src/main/java/com/ibm/config/AuthorizationServerConfig.java
@@ -0,0 +1,17 @@
+package com.ibm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("clientapp").secret("clientpwd").authorizedGrantTypes("authorization_code")
+            .scopes("read").authorities("CLIENT");
+    }
+}
